153e47f621107d3a29b747a6f77aaa8ce82c2978
NoCaliano <114477319+NoCaliano@users.noreply.github.com>
2022-11-18 23:06:55 +0200

    ewd

--- test.cpp
+++ test.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int x = 0;
+	for (int i = 0; i < 1000; i++) 
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			x = x + i;
+			
+		
+	}
+	cout << x << endl;
+}
